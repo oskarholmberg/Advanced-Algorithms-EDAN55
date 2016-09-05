0312652ce69fd5e795c75c37b419f2496447bb49
joacim <joacim.astrom@gmail.com>
2016-09-05 11:47:54 +0200

    knuth efficiency with map and array

--- src/MarkingTree/KnuthArray.java
+++ src/MarkingTree/KnuthArray.java
@@ -1,24 +1,41 @@
 package MarkingTree;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
-public class KnuthArray extends ArrayList<Integer> {
+public class KnuthArray {
+
+	private int[] list;
+	private int last;
+	private HashMap<Integer, Integer> map;
+
+	public KnuthArray(int i) {
+		list = new int[i];
+		map = new HashMap<Integer, Integer>();
+		last = i - 1;
+		fillArray(i);
+	}
 
 	public int popRandom() {
-		int index = (int) Math.ceil(Math.random() * size());
-		int chosen = get(index);
-		markNode(index);
+		int index = (int) Math.ceil(Math.random() * last);
+		int chosen = list[index];
+		shiftList(index);
 		return chosen;
 	}
 
 	public void markNode(int i) {
-		shiftList(indexOf(i));
+		shiftList(map.get(i));
 	}
 
 	private void shiftList(int i) {
-		set(i, get(size() - 1));
-		remove(size() - 1);
+		list[i] = list[last];
+		map.put(list[i], i);
+		last--;
 	}
-	
-	
+
+	private void fillArray(int i) {
+		for (int k = 1; k <= i; k++) {
+			list[k - 1] = k;
+			map.put(k, k - 1);
+		}
+	}
 }
