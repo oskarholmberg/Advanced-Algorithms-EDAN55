f2f5c207deb3bf2abda455e269f9b16ac694d476
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-05 13:11:22 +0200

    asd

--- src/MarkingTree/Tree.java
+++ src/MarkingTree/Tree.java
@@ -4,27 +4,39 @@ import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Tree {
-	public static int depth = 5;
+	public static final int depth = 5;
+	public static final int runTimes = 100;
+	public static final boolean print = false;
 	public static Selector k;
 
 	public static void main(String[] args) {
 
-		AtomicInteger at = new AtomicInteger(0);
 
-		HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
-
-		Node n = new Node(depth-1, null, at, nodes);
-
-		k = new Selector(at.get());
-
-		int count = 0;
-		while (!k.isEmpty()) {
-			count++;
-			int recieved = k.restPop();
-            if(!nodes.get(recieved).marked) nodes.get(recieved).mark(true);
-			TreePrinter.printNode(n);
+		
+		double[] data = new double[runTimes];
+		for (int i = 0; i < runTimes; i++){
+			int count = 0;
+			
+			AtomicInteger at = new AtomicInteger(0);
+			HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+			Node n = new Node(depth-1, null, at, nodes);
+			k = new Selector(at.get());
+			
+			
+			while (!k.isEmpty()) {
+				count++;
+				int recieved = k.restPop();
+	            if(!nodes.get(recieved).marked) nodes.get(recieved).mark(true);
+	            if (print)
+	            	TreePrinter.printNode(n);
+			}
+			data[i] = count;
 		}
-		System.out.println("Covering the tree took: " + count);
+		Statistics s = new Statistics(data);
+		
+		System.out.println("Mean: " + s.getMean() + " Variance: " + s.getVariance());
+		
+//		System.out.println("Covering the tree took: " + count);
 
 	}
 }
