dc5750f01963fc7a23ba1f08cf88c69e9358cd4a
Oskar Holmberg <oskarholmberg@msn.com>
2016-09-05 11:56:04 +0200

    Added printing

--- src/MarkingTree/Main.java
+++ src/MarkingTree/Main.java
@@ -4,4 +4,5 @@ package MarkingTree;
  * Created by oskar on 9/5/16.
  */
 public class Main {
+
 }
--- src/MarkingTree/Tree.java
+++ src/MarkingTree/Tree.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Tree {
-	public static int depth = 10;
+	public static int depth = 3;
 
 	public static void main(String[] args) {
 		AtomicInteger at = new AtomicInteger(0);
@@ -14,8 +14,11 @@ public class Tree {
 
 		Node n = new Node(depth, null, at, nodes, k);
 
-		while (!k.isEmpty()) {
-			nodes.get(k.popRandom()).mark();
-		}
+        TreePrinter tp = new TreePrinter();
+
+        while (!k.isEmpty()) {
+            nodes.get(k.popRandom()).mark();
+            tp.printNode(n);
+        }
 	}
 }
