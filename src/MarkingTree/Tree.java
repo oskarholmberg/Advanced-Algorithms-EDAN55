f15c4674ff55d7f20922a58ff002afd7f16c0347
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-05 11:03:21 +0200

    index

--- src/MarkingTree/Tree.java
+++ src/MarkingTree/Tree.java
@@ -1,8 +1,11 @@
 package MarkingTree;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Tree {
 public static int depth = 10;
 	public static void main(String[] args){
-		Node n = new Node(depth, null);
+		AtomicInteger at = new AtomicInteger(0);
+		Node n = new Node(depth, null, at);
 	}
 }
