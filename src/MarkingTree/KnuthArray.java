3b4d37c182b06defb0ab7fd8cb93266f16378138
joacim <joacim.astrom@gmail.com>
2016-09-05 11:16:36 +0200

    knuth fix

--- src/MarkingTree/KnuthArray.java
+++ src/MarkingTree/KnuthArray.java
@@ -3,17 +3,22 @@ package MarkingTree;
 import java.util.ArrayList;
 
 public class KnuthArray extends ArrayList<Integer> {
-	
+
 	public int popRandom() {
 		int index = (int) Math.ceil(Math.random() * size());
 		int chosen = get(index);
 		markNode(index);
 		return chosen;
-}
+	}
 
 	public void markNode(int i) {
-		set(i, get(size()-1));
-		remove(size() - 1);
-}
+		shiftList(indexOf(i));
+	}
 
+	private void shiftList(int i) {
+		set(i, get(size() - 1));
+		remove(size() - 1);
+	}
+	
+	
 }
