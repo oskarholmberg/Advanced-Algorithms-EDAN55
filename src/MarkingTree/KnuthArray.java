4c7505ce1a9997f2a58bc75668f24b2cef2d1f68
joacim <joacim.astrom@gmail.com>
2016-09-05 11:10:25 +0200

    knuthlist

---
+++ src/MarkingTree/KnuthArray.java
@@ -0,0 +1,19 @@
+package MarkingTree;
+
+import java.util.ArrayList;
+
+public class KnuthArray extends ArrayList<Integer> {
+	
+	public int popRandom() {
+		int index = (int) Math.ceil(Math.random() * size());
+		int chosen = get(index);
+		markNode(index);
+		return chosen;
+}
+
+	public void markNode(int i) {
+		set(i, get(size()-1));
+		remove(size() - 1);
+}
+
+}
