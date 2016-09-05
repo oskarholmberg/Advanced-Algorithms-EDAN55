59a07b99c2c371a29512504a78b58d4580dbc2af
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-05 12:06:56 +0200

    fix

--- src/MarkingTree/Tree.java
+++ src/MarkingTree/Tree.java
@@ -5,19 +5,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Tree {
 	public static int depth = 3;
+	public static KnuthArray k;
 
 	public static void main(String[] args) {
-		AtomicInteger at = new AtomicInteger(0);
+		AtomicInteger at = new AtomicInteger(1);
 
 		HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
 
 		Node n = new Node(depth, null, at, nodes);
 
-		KnuthArray k = new KnuthArray(at.get());
+		k = new KnuthArray(at.get());
 
 		while (!k.isEmpty()) {
 			nodes.get(k.popRandom()).mark();
 			TreePrinter.printNode(n);
 		}
+		//TreePrinter.printNode(n);
+
 	}
 }
