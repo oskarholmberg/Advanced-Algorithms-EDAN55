c5eda3e5df8b579a0167a15887a077202b0d8e53
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-12 12:25:29 +0200

    asd

---
+++ src/MaxCut/Edge.java
@@ -0,0 +1,5 @@
+package MaxCut;
+
+public class Edge {
+
+}
---
+++ src/MaxCut/Main.java
@@ -0,0 +1,20 @@
+package MaxCut;
+
+public class Main {
+	public static final int runTimes = 100;
+	public static void main(){
+		double bestResult = Double.MIN_VALUE;
+		
+		for (int i = 0; i < runTimes; i++){
+			
+		}
+		
+		
+		
+		
+	}
+	
+	public static double getValueOfCut(){
+		return 0;
+	}
+}
