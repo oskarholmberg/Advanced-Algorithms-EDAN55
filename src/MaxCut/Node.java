348abba1ce722c3ea8e2ea24b6617cddcd262a6a
Oskar Holmberg <oskarholmberg@msn.com>
2016-09-12 12:57:22 +0200

    added parser code. changed node list to hashmap

--- src/MaxCut/Node.java
+++ src/MaxCut/Node.java
@@ -4,4 +4,18 @@ package MaxCut;
  * Created by oskar on 9/12/16.
  */
 public class Node {
+
+    private int id;
+
+    public Node(int id) {
+        this.id = id;
+    }
+
+    public boolean equals(Object obj) {
+        return obj instanceof Node && ((Node) obj).getId() == id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
---
+++ src/MaxCut/Parser.java
@@ -0,0 +1,64 @@
+package MaxCut;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by oskar on 9/12/16.
+ */
+public class Parser {
+
+    private HashMap<Integer, Node> nodes;
+    private ArrayList<Edge> edges;
+
+    public Parser() {
+        nodes = new HashMap<>();
+        edges = new ArrayList<>();
+    }
+
+    public void parse(String filename) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line = br.readLine();
+            String[] data = line.split(" ");
+            while (line != null) {
+                line = br.readLine();
+                data = line.split(" ");
+                Node n1;
+                Node n2;
+                if (nodes.containsKey(Integer.valueOf(data[0])))
+                    n1 = nodes.get(Integer.valueOf(data[0]));
+                else {
+                    n1 = new Node(Integer.valueOf(data[0]));
+                    nodes.put(Integer.valueOf(data[0]), n1);
+                }
+
+                if (nodes.containsKey(Integer.valueOf(data[1])))
+                    n2 = nodes.get(Integer.valueOf(data[1]));
+                else {
+                    n2 = new Node(Integer.valueOf(data[1]));
+                    nodes.put(Integer.valueOf(data[1]), n2);
+                }
+
+                edges.add(new Edge());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Node> getNodes() {
+        return nodes;
+    }
+
+    public ArrayList<Edge> getEdges() {
+        return edges;
+    }
+}
