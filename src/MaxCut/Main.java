206fef74a0d011fe7cb2159801cf87137ab7447c
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-12 13:34:29 +0200

    asd

--- src/MaxCut/Main.java
+++ src/MaxCut/Main.java
@@ -1,4 +1,5 @@
 package MaxCut;
+
 import java.util.Collection;
 import java.util.Random;
 
@@ -6,25 +7,35 @@ public class Main {
 	public static final int runTimes = 1000;
 
 	public static void main(String[] args) {
-		double bestResult = Double.MIN_VALUE;
+		int bestResult = Integer.MAX_VALUE;
 
 		Parser parser = new Parser();
-		parser.parse("src/MaxCut/matching_1000.txt");
-//		parser.parse("src/MaxCut/pw09_100.9.txt");
+		parser.parse("src/MaxCut/pw09_100.9.txt");
+		// parser.parse("src/MaxCut/pw09_100.9.txt");
 
 		Collection<Node> nodes = parser.getNodes().values();
 		Collection<Edge> edges = parser.getEdges();
-		
+		// int[] histogram = new int[edges.size()];
+		// for (int i = 0; i < histogram.length; i++){
+		// histogram[i] = 0;
+		// }
+
 		for (int i = 0; i < runTimes; i++) {
 			randomizeNodes(nodes);
-			bestResult = Math.max(bestResult, getValueOfCut(edges));
+			int currentResult = getValueOfCut(edges);
+			// System.out.println(currentResult);
+			// histogram[currentResult]++;
+			bestResult = Math.max(bestResult, currentResult);
 		}
 		System.out.println(bestResult);
+		// for (int i = 0; i < histogram.length; i++){
+		// System.out.println(histogram[i]);
+		// }
 
 	}
 
-	public static double getValueOfCut(Collection<Edge> edges) {
-		double total = 0;
+	public static int getValueOfCut(Collection<Edge> edges) {
+		int total = 0;
 		for (Edge e : edges) {
 			if (e.n1.location != e.n2.location)
 				total += e.weight;
@@ -35,7 +46,7 @@ public class Main {
 	public static void randomizeNodes(Collection<Node> nodes) {
 		Random r = new Random();
 		for (Node n : nodes) {
-			n.location = r.nextInt(1);
+			n.location = r.nextInt(2);
 		}
 	}
 }
