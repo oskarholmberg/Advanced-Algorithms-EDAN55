2ea496fbd5a1af6ef497b503e4c7fc354e114022
erikmunkby <erik.munkby@gmail.com>
2016-09-21 10:49:25 +0200

    first revision of algorithm implementation MIS

--- src/MaxIndSet/Main.java
+++ src/MaxIndSet/Main.java
@@ -1,7 +1,76 @@
 package MaxIndSet;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by oskar on 9/20/16.
  */
 public class Main {
+    static int[][] vMatrix;
+    public static void main(String[] args){
+
+    }
+
+    public static int compute(int[][] matrix){
+        int[] matrixCheck = checkMatrixForVertex(matrix);
+        if (matrixCheck[1] == 0){
+            matrix = removeIndex(matrix, matrixCheck[0]);
+            return 1 + compute(matrix);
+        }
+        return Math.max(isIncluded(matrix, matrixCheck[0]), notIncluded(matrix, matrixCheck[0]));
+    }
+
+    public static int isIncluded(int[][] matrix, int index){
+        int[] neighbours = new int[matrix[1].length];
+        for (int i = 0; i < matrix[index].length; i++){
+            if (matrix[index][i] == 1){
+                matrix = removeIndex(matrix, i);
+            }
+        }
+        matrix = removeIndex(matrix, index);
+        return 1 + compute(matrix);
+
+    }
+
+    public static int notIncluded(int[][] matrix, int index){
+        return compute(matrix);
+    }
+
+    public static int[][] removeIndex(int[][] matrix, int index){
+        for (int i = 0; i < matrix[1].length; i++){
+            if (i == index){
+                for (int j = 0; j < matrix[i].length; j++){
+                    matrix[i][j]=-1;
+                }
+            } else if (matrix[i][index] == 1){
+                matrix[i][index] = -1;
+            }
+        }
+        return matrix;
+    }
+
+    public static int[] checkMatrixForVertex(int[][] matrix){
+        int nbrNeighbours = 0;
+        int index = -1;
+        for (int i = 0; i < matrix[1].length; i++) {
+            int temp = neighbourCheck(matrix, i);
+            if(temp == 0){
+                return new int[]{i, 0};
+            }else if (temp>nbrNeighbours) {
+                nbrNeighbours = temp;
+                index=i;
+            }
+        }
+        return new int[]{index, nbrNeighbours};
+    }
+
+    public static int neighbourCheck(int[][] matrix, int index){
+        int temp = 0;
+        for (int i = 0; i < matrix[1].length; i++){
+            if (matrix[index][i] != -1){
+                temp+=matrix[index][i];
+            }
+        }
+        return temp;
+    }
 }
