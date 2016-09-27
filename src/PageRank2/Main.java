b79cbaf3777b1b97b5d39576213b014156c4127c
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-27 12:01:59 +0200

    asd

--- src/PageRank2/Main.java
+++ src/PageRank2/Main.java
@@ -3,6 +3,8 @@ package PageRank2;
 import org.jblas.DoubleMatrix;
 import org.jblas.FloatMatrix;
 
+import PageRank.LinearAlgebra;
+
 /**
  * Created by erik on 27/09/16.
  */
@@ -11,17 +13,16 @@ public class Main {
     public int[][] hyperlink;
     public static void main(String[] args){
 
-        FloatMatrix floatMatrix = Parser.parse("src/PageRank2/Data/three.txt");
-
+        //FloatMatrix floatMatrix = Parser.parse("src/PageRank2/Data/three.txt");
+    	FloatMatrix floatMatrix = LinearAlgebra.getMatrix("src/PageRank2/Data/three.txt");
         for (int i = 0; i < floatMatrix.rows; i++){
             for (int j = 0; j < floatMatrix.columns; j++){
                 System.out.print(floatMatrix.get(i, j) + " ");
             }
             System.out.println();
         }
-      //  FloatMatrix hyperlinks = FloatMatrix.ones(floatMatrix.rows, floatMatrix.columns)/floatMatrix;
 
-
+        
     }
 
     public static float[][] getHyperLink(int[][] matrix){
