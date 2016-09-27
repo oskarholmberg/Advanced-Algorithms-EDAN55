1e401f3aa9b2a2371366b317eefe8d0d14d7ce77
erikmunkby <erik.munkby@gmail.com>
2016-09-27 10:27:27 +0200

    some code to parser

--- src/PageRank2/Parser.java
+++ src/PageRank2/Parser.java
@@ -1,7 +1,23 @@
 package PageRank2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by erik on 27/09/16.
  */
 public class Parser {
+    public static int[][] parse(String fileName){
+
+        BufferedReader br;
+
+        try{
+            br = new BufferedReader(new FileReader(fileName));
+        } catch (IOException e){
+
+        }
+        return null;
+    }
 }
