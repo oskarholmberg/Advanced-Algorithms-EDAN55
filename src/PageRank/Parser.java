dc522584324cd85964a0458f25c2efb650559172
oskarholmberg <dat12oho@student.lu.se>
2016-09-27 10:25:45 +0200

    Flyttade input till datamapp

--- src/PageRank/Parser.java
+++ src/PageRank/Parser.java
@@ -11,32 +11,32 @@ import java.util.List;
 public class Parser {
 
 	
-	public static List<Node> parse(String path){
+	public static List<Node> parse(String path) {
 		List<Node> nodes = new ArrayList<Node>();
-		
-			BufferedReader br;
-			try {
-				br = new BufferedReader(new FileReader(path));
+
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(path));
 
 			String line = br.readLine();
 			int size = Integer.parseInt(line);
-			
-			for (int i = 0; i < size; i++){
+
+			for (int i = 0; i < size; i++) {
 				nodes.add(new Node());
 			}
-			
+
 			line = br.readLine();
 			String[] data;
 			while (line != null) {
 				data = line.split(" ");
-				for (int i = 0; i < data.length; i+=2){
-					
+				for (int i = 0; i < data.length; i += 2) {
+
 				}
 				line = br.readLine();
 			}
 		} catch (IOException e) {
-				e.printStackTrace();
-			}
-	
+			e.printStackTrace();
 		}
+
+	}
 }
