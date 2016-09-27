2efc6a3ba18b575a1afd97c33d93445cfe64e23f
joacim <joacim.astrom@gmail.com>
2016-09-27 10:04:39 +0200

    randomizer

--- src/PageRank/Main.java
+++ src/PageRank/Main.java
@@ -1,5 +1,27 @@
 package PageRank;
 
+import java.util.HashSet;
+
 public class Main {
 
+	public Node[] nodes;
+	
+	public static void main(String[] args) {	
+		
+		
+		
+		
+	}
+	
+	public Node jumpNext(Node n) {
+		Node nextNode;
+		if (n.edges.size() == 0) {
+				nextNode = nodes[(int) Math.floor(Math.random() * nodes.length)];
+				
+			} else {
+				nextNode = n.edges.get((int) Math.floor(Math.random() * n.edges.size())).node;
+			}
+			return nextNode;
+		}
+	
 }
--- src/PageRank/Node.java
+++ src/PageRank/Node.java
@@ -4,5 +4,14 @@ import java.util.Collection;
 import java.util.List;
 
 public class Node {
+	
+	public int score;
 	public List<Edge> edges;
+	
+	
+	
+	
+	public void increment() {
+		score++;
+	}
 }
