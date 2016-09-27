fe766648b781d649b06e8373fb5dd05d471e78eb
erikmunkby <erik.munkby@gmail.com>
2016-09-27 11:24:50 +0200

    continuing on main method

--- src/PageRank2/Main.java
+++ src/PageRank2/Main.java
@@ -4,9 +4,11 @@ package PageRank2;
  * Created by erik on 27/09/16.
  */
 public class Main {
+    public int[][] adjecency;
+    public int[][] hyperlink;
     public static void main(String[] args){
 
-        int[][] transMatrix = Parser.parse("src/PageRank2/Data/three.txt");
+        int[][] transMatrix = Parser.parse("src/PageRank2/Data/medium.txt");
 
         for (int i = 0; i < transMatrix[0].length; i++){
             for (int j = 0; j < transMatrix[0].length; j++){
@@ -14,5 +16,7 @@ public class Main {
             }
             System.out.println();
         }
+
+
     }
 }
--- src/PageRank2/Parser.java
+++ src/PageRank2/Parser.java
@@ -3,7 +3,6 @@ package PageRank2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Created by erik on 27/09/16.
