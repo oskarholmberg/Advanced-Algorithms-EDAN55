a92cd558aba8951e25dfd025b2becf4604135c00
Tobias Elinder <tobias.elinder@lunicore.se>
2016-09-29 08:59:37 +0200

    asd

--- src/PageRank/LinearAlgebra.java
+++ src/PageRank/LinearAlgebra.java
@@ -8,7 +8,7 @@ import org.ejml.simple.SimpleMatrix;
 public class LinearAlgebra {
 
 	public static void main(String[] args) {
-		String path = "src/PageRank/Data/wikipedia.txt";
+		String path = "src/PageRank/Data/p2p-Gnutella08-mod.txt";
 		
 		List<Node> nodes = Parser.parse(path);
 		double[] startVector = new double[nodes.size()];
@@ -34,10 +34,11 @@ public class LinearAlgebra {
 
 	private static boolean testChange(double[] prev, double[] curr, double d) {
 		for (int i = 0; i < prev.length; i++) {
-			if (Math.abs(prev[i] - curr[i]) > d){
+			if (Math.abs((curr[i] - prev[i])/prev[i]) > d){
 				return true;
 			}
 		}
+
 		return false;
 	}
 
