f51edd5ba0a8e100daf62770e58d2277b2086264
oskarholmberg <dat12oho@student.lu.se>
2016-10-04 12:02:00 +0200

    Started parser work

--- src/TreeWidth/Parser.java
+++ src/TreeWidth/Parser.java
@@ -4,4 +4,16 @@ package TreeWidth;
  * Created by oskar on 2016-10-04.
  */
 public class Parser {
+
+    public Parser(){
+
+    }
+
+    public void parseGraph(String path){
+
+    }
+
+    public void parseTree(){
+
+    }
 }
