19de4909a34b1310b586c200ddb34a0bf89826b1
Tobias Elinder <tobias.elinder@lunicore.se>
2016-10-04 14:54:11 +0200

    asd

--- src/TreeWidth/Algorithm.java
+++ src/TreeWidth/Algorithm.java
@@ -3,44 +3,77 @@ package TreeWidth;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import IndependentSet.Node;
 
 public class Algorithm {
 	
 
 
 	public static void main(String[] args){
-//		List<SubTree> trees = null;
-//		List<Node> nodes = null;
-//		
-//		// precalculate partial solutions
-//		for (SubTree tree: trees){
-//			tree.calculate(calculatedSolutions);
-//		}
-//		
-//		// normal ind. set algorithm
-//		
+		List<Bag> bags = null;
+		List<Node> nodes = null;
+		
+		// precalculate partial solutions
+		for (Bag bag: bags){
+			bag.calculatesolutions();
+		}
+		
+		// normal ind. set algorithm
+		
 //		getIndependentSet(nodes, new ArrayList<Node>());		
-//		
-//	}
-//
-//	private static List<Node> getIndependentSet(List<Node> nodes, List<Node> independentSet) {
-//		for (Node n : nodes){
-//			if (n.getNeighbours().size() == 0){
-//				independentSet.add(n);
-//				nodes.remove(n);
-//				return getIndependentSet(nodes, independentSet);
-//			}
-//		}
-//		
-//		Node mostNeighbors
-//		for ()
-//		
-//		for (Node n : nodes){
-//			
-//		}
+		
+	}
+
+	public static List<Node> getIndependentSet(Set<Node> remaining, List<Node> independentSet) {
+		if (remaining.size() == 0){
+			return independentSet;
+		}
+		for (Node n : remaining){
+			if (n.getNeighbours().size() == 0){
+				independentSet.add(n);
+				Set<Node> remainingCopy = new HashSet<Node>(remaining);
+				remainingCopy.remove(n);
+				independentSet.add(n);
+				return getIndependentSet(remainingCopy, independentSet);
+				//independentSet.remove(n);
+				
+			}
+		}
+		
+		Node mostConn = null;
+		int mostConnNbr = 0;
+		for (Node n : remaining){
+			if (getValidNeighbors(n.getNeighbours(), remaining) > mostConnNbr){
+				mostConn = n;
+				mostConnNbr = n.getNeighbours().size();
+			}
+		}
+		
+		// split
+		
+		// try adding to set
+		
+		Node remaining = 0;
+		
+		
+		// try removing
+		
+		
+		return independentSet;
+	}
+	
+	public static int getValidNeighbors(List<Node> neighbors, Set<Node> remaining){
+		int nbr = 0;
+		for (Node n : neighbors){
+			if (remaining.contains(n)){
+				nbr++;
+			}
+		}
+		return nbr;
+		
 	}
 
 }
--- src/TreeWidth/Bag.java
+++ src/TreeWidth/Bag.java
@@ -1,21 +1,47 @@
 package TreeWidth;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Bag {
-	public List<Node> nodes;
+	public Set<Node> nodes;
 	public List<Node> edgeNodes;
 	//public List<SubTree> neighboringTrees;
 	public HashMap<BitSet, List<Node>>  partialSolutions;
 	
-	public void calculate(HashMap<Integer, BitSet> calculatedSolutions) {
-		for (Node n : nodes){
-			for (Node n2 : n.getNeighbours()){
-				
-			}
-		}
+	
+	public Bag(){
 		
 	}
+
+	public void calculatesolutions() {
+//		for (Node n : edgeNodes){
+			Set<Node> remainingCopy = new HashSet<Node>();
+			remainingCopy.remove(n);
+			
+			ArrayList<Node> indepSet = new ArrayList<Node>();
+			indepSet.add(n);
+			
+			List<Node> result = Algorithm.getIndependentSet(remainingCopy, new ArrayList<Node>());
+			
+			BitSet currSet = new BitSet(edgeNodes.size());
+			for (int i = 0; i < edgeNodes.size(); i++){
+				currSet.set(i, result.contains(edgeNodes.get(i)));
+			}
+			
+			partialSolutions.put(currSet, result);
+			
+//		}
+		
+	}
+	
+	public void testCombinations(List<Node> remainingEdgeNodes){
+		ArrayList<>
+	}
+	
+	
 }
