54733efc3b747f5c54e4b6b3048c4b1c6508307d
oskarholmberg <dat12oho@student.lu.se>
2016-10-06 08:35:20 +0200

    FileFInder

--- src/PageRank/Main.java
+++ src/PageRank/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static String path;
 	
 	public static void main(String[] args) {	
-		path = "src/PageRank/Data/three.txt";
+		path = "src/PageRank/Data/medium.txt";
 
 		damping = 0.85;
 
